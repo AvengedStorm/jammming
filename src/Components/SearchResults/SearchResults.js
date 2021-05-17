30ba7efb24c189db2a83378971f1b959089a745c
omri <DJCreeperNo1@gmail.com>
2021-05-17 13:19:37 +0300

    regular commit (not finished)

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './SearchResults';
-import Tracklist from './../TrackList/Tracklist.js';
+import './SearchResults.css';
+import TrackList from '../TrackList/TrackList.js';
 
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <Tracklist />
+                <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false} />
             </div>
         )
     }
 }
 
-export default {SearchResults};
+export default SearchResults;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import Track from './../Track/Track.js'
+import './TrackList.css';
+import Track from '../Track/Track';
 
 class Tracklist extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                You will add a map method that renders a set of Track components
+                {
+                    this.props.tracks.map(track => {
+                        return <Track   track={track} 
+                                        key={track.id} 
+                                        onAdd={this.props.onAdd}
+                                        onRemove={this.props.onRemove}
+                                        isRemoval={this.props.isRemoval}
+                                />
+                    })
+                }
             </div>
         )
     }
 }
 
-export default {Tracklist};
+export default Tracklist;
